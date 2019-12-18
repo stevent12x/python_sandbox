e2793fb8edef2bd837eec5dd5395904f262d4627
Steven Turner <steven.turner12x@gmail.com>
2019-12-18 17:50:34 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hello_python_world',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='steventurner',
+    author_email='',
+    description='my first python program'
+)
